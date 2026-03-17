65d37c25dcecba2b9942280026ae1093aceee084
Ashley Flynn <ashnflynn@gmail.com>
2026-03-16 20:54:43 -0500

    day 1 breakout, set up

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// function weapon(name, dmg, durability) {
+//     this.name = name;
+//     this.dmg = dmg;
+//     this.durability = durability;
+// }
+
+// const crowbar = new weapon("Crowbar", 12, 100)
+// console.log(crowbar)
+
+
+class Weapon {
+    constructor() {
+        this.name
+        this.dmg
+        this.durability
+    }
+}
+
+const crowbar = new Weapon("Crowbar", "12", "100")
+
+console.log(crowbar.name)
